cd769bbe78956791ffdb75b1a2ed557b537b756d
isaac chavez <isaacgc0596@gmail.com>
2020-02-02 03:00:49 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ end
 def print_first_directors_movie_titles
   row_index = 0 
   while row_index < directors_database[0][:movies].length do
-    puts directors_database[0][:movies][row_index][:title]
+    puts directors_database[0][:movie][row_index][:title]
   row_index += 1
 end
 end
